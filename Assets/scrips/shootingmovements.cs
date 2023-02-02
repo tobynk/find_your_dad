df0345bb01b52a5ccc7742ea09a4322bb6eaf900
tobynk <122053994+tobynk@users.noreply.github.com>
2023-02-02 12:09:52 -0800

    Lab 2 - New Project with Primitives

--- Assets/scrips/shootingmovements.cs
+++ Assets/scrips/shootingmovements.cs
@@ -11,7 +11,7 @@ public class shootingmovements : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         Destroy(gameObject, 5.0f);
+         Destroy(gameObject, 1.0f);
     }
 
     // Update is called once per frame
