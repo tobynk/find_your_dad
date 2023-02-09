f15912af564cbd0af0ea8a4e66391d8421edbb5f
tobynk <122053994+tobynk@users.noreply.github.com>
2023-02-09 12:40:59 -0800

    woeking on https://www.youtube.com/watch?v=4HpC--2iowE
    https://www.youtube.com/watch?v=4HpC--2iowE

--- Assets/scrips/playermovemnet.cs
+++ Assets/scrips/playermovemnet.cs
@@ -2,33 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+using Quaternion = UnityEngine.Quaternion;
 
 public class playermovemnet : MonoBehaviour
 {
-    private float speed = 10.0f;
+    public float speed = 10.0f;
     private float jumpSpeed = 5f;
     private float dashSpeed = 20f;
     private float horizontalInput;
     private float verticalInput; 
     private float fowardinput;
     public GameObject shootingE;
+    public CharacterController controller;
+    public float turnsmoothtime=0.1f;
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        fowardinput = Input.GetAxis("Vertical");
-        //making the car go
-        transform.Translate(UnityEngine.Vector3.forward * Time.deltaTime * fowardinput*speed);
-        transform.Translate(UnityEngine.Vector3.right * Time.deltaTime * horizontalInput*speed);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        //transform.Translate(UnityEngine.Vector3.forward * Time.deltaTime * fowardinput*speed);
+        //transform.Translate(UnityEngine.Vector3.right * Time.deltaTime * horizontalInput*speed);
         
+        if(direction.magnitude >=0.1f)
+        {
+            float tragetAngle = Mathf.Atan2(direction.x,direction.z)*Mathf.Rad2Deg;
+            float angle=Mathf.SmoothDampAngle(transform.eulerAngles.y, tragetAngle, ref turnsmoothtime)
+            transform.rotation = Quaternion.Euler(0f,tragetAngle,0f);
+            controller.Move(direction*speed*Time.deltaTime);
+        }
         if (Input.GetKeyDown(KeyCode.E))
         {
             Instantiate(shootingE, transform.position,shootingE.transform.rotation);
         }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-        }
+        
 
     }
 }
