88a844263876d7b7701a450eb13de680af460f3e
tobynk <122053994+tobynk@users.noreply.github.com>
2023-04-07 12:43:09 -0700

    eadhfisudhfklfadsfdsgs

--- Assets/scrips/emenry/healthbarem.cs
+++ Assets/scrips/emenry/healthbarem.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class healthbarem : MonoBehaviour
 {
-    public slider emslider;
+    public Slider emslider;
+    public healthbar healthBar;
 
     public void SetMaxHealth(int emmaxHealth)
     {
