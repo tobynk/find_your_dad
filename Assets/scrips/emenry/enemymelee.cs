5cd24872ca095204eba1c2ff8696895ca7021373
dav <usr21p1@gmail.com>
2023-04-24 12:02:38 -0700

    Fix from merge

--- Assets/scrips/emenry/enemymelee.cs
+++ Assets/scrips/emenry/enemymelee.cs
@@ -8,6 +8,7 @@ public class enemymelee : MonoBehaviour
     public float stoppingDistance = 0f; // Distance at which the enemy stops moving towards the player
     public int damage=100;
     public float rechargeTime = 1.0f;
+    public float speed = 1.0f;
 
     private takehealth playerHealth;
     private Transform playerTransform;   // Reference to the player's transform
@@ -22,12 +23,11 @@ public class enemymelee : MonoBehaviour
 
     private void Update() {
         // Calculate the distance between the enemy and the player
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-        Debug.Log(distanceToPlayer);
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
 
         // If the enemy is not within the stopping distance of the player, move towards the player
         if (distanceToPlayer > stoppingDistance) {
-            Vector3 direction = player.position - transform.position;
+            Vector3 direction = playerTransform.position - transform.position;
             direction.Normalize();
             transform.position += direction * speed * Time.deltaTime;
         }
